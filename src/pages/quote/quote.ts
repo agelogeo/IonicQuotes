fb23c5870092a7377a43b98d55c8719c1df46539
agelogeo (PC) <agelo1995@gmail.com>
2017-08-28 12:07:40 +0300

    S3L59

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,25 +1,15 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {ViewController} from "ionic-angular";
 
-/**
- * Generated class for the QuotePage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html',
 })
 export class QuotePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(private viewCtrl: ViewController){}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
+  onClose(){
+    this.viewCtrl.dismiss();
   }
-
 }
